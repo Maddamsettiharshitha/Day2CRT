f8e602f36dd820b1df403df8a8408db06c1bda29
M.Harshitha <130836745+Maddamsettiharshitha@users.noreply.github.com>
2024-12-10 16:06:17 +0530

    Create code1.js

---
+++ code1.js
@@ -0,0 +1,47 @@
+// console.log("hello");
+// setTimeout( ()=>console.log("hi"),5000);
+// console.log("world");
+
+// console.log("hello");
+// alert("hi");
+// console.log("world");
+
+// Readfilesync
+// let fs=require("fs");
+// console.log("hello");
+// let readsync=fs.readFileSync("./abc.text","utf-8");
+// console.log("world");
+// console.log(readsync);
+
+//READFILE SYNC
+// let fs=require("fs");
+// console.log("hello");
+// let readsync=fs.readFileSync("./abc.txt","utf-8");
+// console.log(readsync);
+// console.log("world");
+
+
+
+//arrow function
+// let d=(a,b)=>a+b;
+// let g=d(10,20);
+// console.log(g);
+
+// READFILE
+// let fs = require("fs")
+// console.log("hello");
+// let readsync=fs.readFile("./abc.txt","utf-8",(err,data)=>console.log(data));
+// console.log(readsync);
+// console.log("world");
+       
+
+// let http=require("http");
+// let server=http.createServer((req,res)=>
+//      {
+//         console.log(req.url);
+//         res.end("hello");
+//      });
+// let port=8087
+// server.listen(port,"localhost",()=> console.log(`Server is active on localhost: ${port}`)
+// );
+
